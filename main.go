bc5d86b27323fb36aa92ae2632e349cb1f491d5a
Yora Radityohutomo <yoratyo@gmail.com>
2020-04-29 17:29:09 +0700

    add graceful shutdown

--- main.go
+++ main.go
@@ -3,7 +3,11 @@ package main
 import (
 	"log"
 	"time"
+	"context"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/gorilla/mux"
 )
@@ -27,6 +31,25 @@ func main () {
 			log.Fatal(err)
 		}
 	}()
+
+	// Graceful Shutdown
+	waitForShutdown(srv)
+}
+
+func waitForShutdown(srv *http.Server) {
+	interruptChan := make(chan os.Signal, 1)
+	signal.Notify(interruptChan, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
+	// Block until we receive our signal.
+	<-interruptChan
+
+	// Create a deadline to wait for.
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
+	defer cancel()
+	srv.Shutdown(ctx)
+
+	log.Println("Shutting down")
+	os.Exit(0)
 }
 
 func indexHandler (w http.ResponseWriter, r *http.Request) {
